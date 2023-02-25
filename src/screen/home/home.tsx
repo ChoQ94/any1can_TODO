71f3f4daf81f236cef781fd7ee0c5cc314a54450
choq94 <whrbdnjs1994@gmail.com>
2023-02-25 16:23:04 +0900

    feat: prettier 적용

--- src/screen/home/home.tsx
+++ src/screen/home/home.tsx
@@ -1,52 +1,58 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import Image from "next/image";
-import styles from "src/screen/home/styles.module.scss"
-import back from "public/assets/image/background.png"
-import { Typography } from '@mui/material';
-import Input from '@mui/joy/Input';
-import Button from '@mui/joy/Button';
-import { margin } from '@mui/system';
+import styles from "src/screen/home/styles.module.scss";
+import back from "public/assets/image/background.png";
+import { Typography } from "@mui/material";
+import Input from "@mui/joy/Input";
+import Button from "@mui/joy/Button";
 
 export default function Home() {
-    const [text, setText] = useState('');
-    const clear = () => {
-        setText('');
-    }
+  const [text, setText] = useState<string>("");
+  const [todoList, setTodoList] = useState<string[]>([]);
 
-    return(
-        <div className={styles.back}>
-           <Image
-                src={back}
-                fill
-                alt="메인 배경 이미지"
-                style={{zIndex: -99}}
-            />
-            <div className={styles.mainTable}>
-                <div className={styles.title}>
-                        <Typography fontWeight={700}>
-                            TODO LIST ANYONE CAN DO
-                        </Typography>
-                </div>
-                <div className={styles.date}>
-                    <Typography fontSize={50} fontWeight={700}>
-                        Feb 6, 2023
-                    </Typography>
-                    <Typography fontSize={30} fontWeight={700}>
-                        Wed
-                    </Typography>
-                </div>
-                <div className={styles.input}>
-                    <Input
-                        // startDecorator={'ㅋㅋ'}
-                        value={text}
-                        onChange={(e)=>setText(e.target.value)}
-                        placeholder='오늘 나는..'
-                        variant="outlined"
-                        endDecorator={<Button sx={{border: '1px solid white'}} variant='solid' color="primary" onClick={clear}>+</Button>}
-                    ></Input>
-                </div>
-                <div className={styles.divider} />                   
-            </div>
+  const clear = () => {
+    setTodoList([text, ...todoList]);
+    setText("");
+  };
+  console.log(todoList);
+
+  return (
+    <div className={styles.back}>
+      <Image src={back} fill alt="메인 배경 이미지" style={{ zIndex: -99 }} />
+      <div className={styles.mainTable}>
+        <div className={styles.title}>
+          <Typography fontWeight={700}>TODO LIST ANYONE CAN DO</Typography>
         </div>
-    )
+        <div className={styles.date}>
+          <Typography fontSize={50} fontWeight={700}>
+            Feb 6, 2023
+          </Typography>
+          <Typography fontSize={30} fontWeight={700}>
+            Wed
+          </Typography>
+        </div>
+        <div className={styles.input}>
+          <Input
+            // startDecorator={'ㅋㅋ'}
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder="오늘 나는.."
+            variant="outlined"
+            endDecorator={
+              <Button
+                sx={{ border: "1px solid white" }}
+                variant="solid"
+                color="primary"
+                onClick={clear}
+              >
+                +
+              </Button>
+            }
+          ></Input>
+        </div>
+        <div className={styles.divider} />
+        <div className={styles.todoListContainer}></div>
+      </div>
+    </div>
+  );
 }
