0a1e87eedde0c1002de5f1d61b9b97bcf60c5863
ChoQ94 <whrbdnjs1994@gmail.com>
2023-05-03 17:44:28 +0900

    feat: 날짜 설정 함수화

---
+++ src/logics/common.ts
@@ -0,0 +1,10 @@
+const dateConverter = (date: Date) => {
+  const dateFormat =
+    date.getFullYear() +
+    "-" +
+    (date.getMonth() + 1 < 9
+      ? "0" + (date.getMonth() + 1)
+      : date.getMonth() + 1) +
+    "-" +
+    (date.getDate() < 9 ? "0" + date.getDate() : date.getDate());
+};
