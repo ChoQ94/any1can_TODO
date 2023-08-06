6dd3a3f249836dfd16f39222de9294a8c883898c
ChoQ94 <whrbdnjs1994@gmail.com>
2023-08-06 17:00:43 +0900

    feat: webpack 설정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+    config.resolve.alias.canvas = false;
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
